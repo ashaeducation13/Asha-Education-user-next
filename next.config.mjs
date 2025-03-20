a53dfb65dcc499331f46575b84b4750a57f1a260
aswin <aswink.accolades@gmail.com>
2025-03-20 17:42:30 +0530

    import build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    devIndicators: false,
-};
-
-export default nextConfig;
-
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//     images: {
-//         unoptimized: true,
-//       },
-
-//     basePath: '/asha', // Set the basePath for the app
-//     assetPrefix: '/asha', // Ensure static files are correctly referenced
 //     devIndicators: false,
 // };
 
 // export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        unoptimized: true,
+      },
+
+    basePath: '/asha', // Set the basePath for the app
+    assetPrefix: '/asha', // Ensure static files are correctly referenced
+    devIndicators: false,
+};
+
+export default nextConfig;
