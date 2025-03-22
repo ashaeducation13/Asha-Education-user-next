a8681cec041f4a670d2eb0e7e946f2e967fb1c7f
aswin <aswink.accolades@gmail.com>
2025-03-22 09:54:22 +0530

    uni inner

--- src/app/universities/[id]/Certcard.jsx
+++ src/app/universities/[id]/Certcard.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 const Certcard = ({ imgSrc, title }) => {
     return (
-      <div className="max-w-xs bg-white border border-gray-200 rounded-2xl shadow-lg p-6 flex flex-col items-center text-center">
+      <div className="max-w-xs bg-white py-6 flex flex-col items-center text-center">
         {/* Image */}
-        <div className="w-20 h-20 mb-4">
+        <div className="w-24 h-24 mb-4">
           <Image src={imgSrc} alt={title} className="w-full h-full object-contain" />
         </div>
   
