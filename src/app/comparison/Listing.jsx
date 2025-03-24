bdc251c32a48525e74d847c6f25249bc34560adb
aswin <aswink.accolades@gmail.com>
2025-03-24 15:25:31 +0530

    prgram page, uni page

--- src/app/comparison/Listing.jsx
+++ src/app/comparison/Listing.jsx
@@ -56,8 +56,8 @@ const Listing = () => {
             </ul>
           </div>
           
-          <div className="flex flex-col gap-10">
-            <div className="grid grid-cols-2  xl:grid-cols-3 gap-x-5 gap-y-10">
+          <div className="flex flex-col gap-10 items-center">
+            <div className="grid grid-cols-2 gap-x-5 gap-y-10">
               {cardData.map((item, index) => (
                 <Card key={index} item={item} />
               ))}
